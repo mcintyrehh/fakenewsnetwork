f8f31f5890c8f1b0622d26ced21c679a099aa7d3
Onray <mcintyrehh@gmail.com>
2018-10-27 12:20:09 -0400

    update to include accessibility for emojis

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
-import Wrapper from '../../components/Wrapper'
+import Wrapper from '../../components/Wrapper';
 import { Row, Col } from 'antd';
-import { Card } from '../../components/Card'
+import { Card } from '../../components/Card';
 import { Menu, Dropdown, Input, Icon } from 'antd';
 import '../../App.css';
-import './Home.css'
+import './Home.css';
 const { Header, Footer, Content } = Layout;
-
-
-
-
 
 class Home extends Component {
     
@@ -30,34 +26,13 @@ class Home extends Component {
         this.setState({ userName: e.target.value });
     }
     render() {
-        const { userName } = this.state;
-        const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
-        const menu = (
-            <Menu>
-                <Menu.Item key="0">
-                    <Input
-                        placeholder="Enter your username"
-                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        suffix={suffix}
-                        value={userName}
-                        onChange={this.onChangeUserName}
-                        ref={node => this.userNameInput = node}
-                    />
-                </Menu.Item>
-                <Menu.Item key="1">
-                    <a href="http://www.taobao.com/">2nd menu item</a>
-                </Menu.Item>
-                <Menu.Divider />
-                <Menu.Item key="3">3rd menu item</Menu.Item>
-            </Menu>
-        );
         return (
             <Wrapper>
                 <Layout>
                     <Header style={{ textAlign: 'right' }}>
                         <Row className="logo">
                             <Col span={8}></Col>
-                            <Col className="title" span={8}>🕵️‍Fake News Network🕵️️</Col>
+                            <Col className="title" span={8}><span role="img" aria-label="investigator emoji">🕵️‍</span>Fake News Network<span role="img" aria-label="investigator emoji">🕵️️</span></Col>
                             <Col span={8}>
 
                                 <Menu
@@ -67,13 +42,7 @@ class Home extends Component {
                                     // defaultSelectedKeys={['2']}
                                     style={{ lineHeight: '64px' }}>
                                     <Menu.Item key="1">Sign Up</Menu.Item>
-                                    <Menu.Item key="2">
-                                        <Dropdown overlay={menu} trigger={['click']}>
-                                            <a className="ant-dropdown-link" href="#">
-                                                Login <Icon type="user" />
-                                            </a>
-                                        </Dropdown>
-                                    </Menu.Item>
+                                    <Menu.Item key="2">Sign In</Menu.Item>
                                     <Menu.Item key="3">Log Out</Menu.Item>
                                 </Menu>
                             </Col>
@@ -90,9 +59,10 @@ class Home extends Component {
                                 <Card></Card>
                                 <Card></Card>
                                 <Card></Card>
-
+                            </Col>/
+                            <Col span={8}>
+                                {/* <SignupForm></SignupForm> */}
                             </Col>
-                            <Col span={8}></Col>
                         </Row>
 
                     </Content>
