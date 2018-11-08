78326f544b0780a1f35a9f0a01f34fc7343a9ee0
GreenEyedOcelot <hbe123@gmail.com>
2018-11-08 15:01:39 -0500

    code cleanup and add src, category, and articleSource

--- server/controllers/scrapeController.js
+++ server/controllers/scrapeController.js
@@ -10,30 +10,22 @@ module.exports = {
         axios.get("http://www.theonion.com").then(response => {
             const $ = cheerio.load(response.data);
             $("article").each(function (i, el) {
-                var sourceId = "onion" + $(el).attr('id').replace(/[^\d]/g, ""); // DB TESTED
-                var theLinks = $('a', $(el));
-                var linkFilterFn = (ind, aLink) => $(aLink).attr('class') && $(aLink).attr('class') === 'js_entry-link';
-                var linkMapFn = (ind, aLink) => $(aLink).attr('href');
-                var titleMapFn = (ind, aLink) => $(aLink).text();
-                var titleNodeArr = theLinks.filter(linkFilterFn);
-                var title = titleNodeArr.map(titleMapFn)[0];
-                var url = titleNodeArr.map(linkMapFn)[0];
-
-
-
-
-                let obj = {};
-                const category = "News in Brief";
-
-                var timeNode = $('time', $(el));
+                let sourceId = "onion" + $(el).attr('id').replace(/[^\d]/g, ""); // DB TESTED
+                let theLinks = $('a', $(el));
+                let linkFilterFn = (ind, aLink) => $(aLink).attr('class') && $(aLink).attr('class') === 'js_entry-link';
+                let linkMapFn = (ind, aLink) => $(aLink).attr('href');
+                let titleMapFn = (ind, aLink) => $(aLink).text();
+                let titleNodeArr = theLinks.filter(linkFilterFn);
+                let title = titleNodeArr.map(titleMapFn)[0];
+                let url = titleNodeArr.map(linkMapFn)[0];
+                let category = "News in Brief";
+                let timeNode = $('time', $(el));
                 var timePublished = new Date(timeNode.attr('datetime'));
-
-                var summaryDivNode = $('div.excerpt', $(el)).get();
-                var summary = $('p', summaryDivNode).text();
-                // TO DO: fix content
-                const content = summary; // for now, this will be updated later to include all of the article
-
-                var pictureNodes = $('picture', $(el));
+                let summaryDivNode = $('div.excerpt', $(el)).get();
+                let summary = $('p', summaryDivNode).text();
+                let content = [summary]; // for now, this will be updated later to include all of the article
+                // For now, the src is just the Onion logo
+                let src = "https://i.kym-cdn.com/entries/icons/facebook/000/010/280/onion.jpg"; // onion logo image
 
                 let data = {
                     sourceId: sourceId,
@@ -41,57 +33,65 @@ module.exports = {
                     url: url,
                     summary: summary,
                     timePublished: timePublished,
-                }
+                    content: content,
+                    category: category,
+                    src: src,
+                    articleSource: "The Onion"
+                };
 
-                // For now, the image is just the onion logo
-                const src = "https://i.kym-cdn.com/entries/icons/facebook/000/010/280/onion.jpg"; // onion logo image
-                //console.log(obj);
-                //console.log(fakeArr);
+                
+      
                 let notInDatabase = 0;
-                console.log(notInDatabase);
+                //console.log(notInDatabase);
                 db.FakeArticles
                     .find()
                     .then(fakeArticles => {
-                        fakeArticles.map(fArticle => {
-                            (fArticle.sourceId !== sourceId) ? notInDatabase++ : console.log("Found");
+                        fakeArticles.forEach(fArticle => {
+                            if (fArticle.sourceId !== sourceId){
+                              notInDatabase++;
+                            } else {
+                              console.log("Found");
+                            } 
                         });
+                        
+                        if (notInDatabase === fakeArticles.length) {
+                           console.log("the length is: ", fakeArticles.length);
 
-                        (notInDatabase === fakeArticles.length) ?
-                            db.FakeArticles
-                                .create(data)
-                                .then(dbFArticle => console.log(dbFArticle))
-                                .catch(err => res.end(err))
-                            : console.log("Article already in Database");
-                    })
+                           db.FakeArticles
+                           .create(data)
+                           .then(dbFArticle => console.log(dbFArticle))
+                           .catch(err => res.end(err));
+                        } else {
+                           console.log("Article already in Database");
+                        }
+                        
+                            
+                         
+                    });
             });
-            res.json({ message: "Scrape Complete" })
+            res.json({ message: "Scrape Complete" });
         });
 
         axios.get("http://www.clickhole.com").then(response => {
             const $ = cheerio.load(response.data);
             $("article").each(function (i, el) {
-                var sourceId = "clickHole" + $(el).attr('id').replace(/[^\d]/g, ""); // DB TESTED
-                var theLinks = $('a', $(el));
-                var linkFilterFn = (ind, aLink) => $(aLink).attr('class') && $(aLink).attr('class') === 'js_entry-link';
-                var linkMapFn = (ind, aLink) => $(aLink).attr('href');
-                var titleMapFn = (ind, aLink) => $(aLink).text();
-                var titleNodeArr = theLinks.filter(linkFilterFn);
-                var title = titleNodeArr.map(titleMapFn)[0];
-                var url = titleNodeArr.map(linkMapFn)[0];
+                let sourceId = "clickHole" + $(el).attr('id').replace(/[^\d]/g, ""); // DB TESTED
+                let theLinks = $('a', $(el));
+                let linkFilterFn = (ind, aLink) => $(aLink).attr('class') && $(aLink).attr('class') === 'js_entry-link';
+                let linkMapFn = (ind, aLink) => $(aLink).attr('href');
+                let titleMapFn = (ind, aLink) => $(aLink).text();
+                let titleNodeArr = theLinks.filter(linkFilterFn);
+                let title = titleNodeArr.map(titleMapFn)[0];
+                let url = titleNodeArr.map(linkMapFn)[0];
+                let category = "News in Brief";
+                let timeNode = $('time', $(el));
+                let timePublished = new Date(timeNode.attr('datetime'));
+                let summaryDivNode = $('div.excerpt', $(el)).get();
+                let summary = $('p', summaryDivNode).text();
+                let content = [summary]; // for now, this will be updated later to include all of the article
 
-
-                let obj = {};
-                const category = "News in Brief";
-
-                var timeNode = $('time', $(el));
-                var timePublished = new Date(timeNode.attr('datetime'));
-
-                var summaryDivNode = $('div.excerpt', $(el)).get();
-                var summary = $('p', summaryDivNode).text();
-                // TO DO: fix content
-                const content = summary; // for now, this will be updated later to include all of the article
-
-                var pictureNodes = $('picture', $(el));
+                 // For now, the src is just the clickhole logo
+                 const src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUkyAHkl2tHsmg7wN07BWT8xEN7BgWUxwWxk0NKM_ZdBeDtfBu"; // clickhole image
 
                 let data = {
                     sourceId: sourceId,
@@ -99,12 +99,13 @@ module.exports = {
                     url: url,
                     summary: summary,
                     timePublished: timePublished,
-                }
+                    content: content,
+                    category: category,
+                    src: src,
+                    articleSource: "Clickhole"
+                };
 
-                // For now, the image is just the click hole logo
-                const src = ""; // click hole logo image
-                //console.log(obj);
-                //console.log(fakeArr);
+           
                 let notInDatabase = 0;
                 console.log(notInDatabase);
                 db.FakeArticles
@@ -120,10 +121,10 @@ module.exports = {
                                 .then(dbFArticle => console.log(dbFArticle))
                                 .catch(err => res.end(err))
                             : console.log("Article already in Database");
-                    })
+                    });
             });
-            res.json({ message: "Scrape Complete" })
+            res.json({ message: "Scrape Complete" });
         });
 
     }
-}
+};
