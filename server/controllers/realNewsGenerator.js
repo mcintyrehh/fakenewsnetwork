760c29d08075dafd994ffda74d8d4a44ec83481b
GreenEyedOcelot <hbe123@gmail.com>
2018-11-10 11:53:41 -0500

    ake date range in realNewsGenerator file dynamic

--- server/controllers/realNewsGenerator.js
+++ server/controllers/realNewsGenerator.js
@@ -1,39 +1,49 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('3f30010330714ea2a7c2f418233d5304');
 
-// Stuff to do:
-// Fix dates - we should decide on a date range
-// Be smart about keywords: try it with first three
+// to do
+// Don't include Onion results
+// // BE SMART ABOUT KEYWORDS: try first 3, and then go in or out accordingly
 
 module.exports = {
    generate: function (req, res) {
+
       let theKeywords = req.body.keywords;
-      console.log("req body is: ", req.body);
+      const articleLimit = 10; // the max number of articles this route will return
+      let getFromAndToDateStrings = (daysBack) => {
+         let toDate = new Date();
+         // the math calculation in fromDate represents (daysBack * hoursInDay * minutesInHour * secondsInMinute * millisecondInSecond)
+         let fromDate = new Date(toDate.getTime() - (daysBack * 24 * 60 * 60 * 1000));
+         let formatNum = monthNum => ((monthNum < 9) ? "0" : "") + (monthNum + 1);
+         let makeFriendlyDate = dateObj => dateObj.getFullYear() + "-" + formatNum(dateObj.getMonth()) + "-" + formatNum(dateObj.getDate());
 
-      let getRealNews = (keywordArr) => {
-         // for now, get news based on first two keywords, this algorithm will be tweaked
-         // for now, date range is hard-coded, this will shortly be fixed.
-         let query = keywordArr.slice(0, 3).join(" ");
-         console.log("query is: ", query);
-
-         newsapi.v2.everything({
-            q: query,
-            from: '2018-11-01',
-            to: '2018-11-10',
-            language: 'en',
-            sortBy: 'relevancy',
-            page: 1
-         }).then(response => {
-            let theReturnedArticles = response.articles.slice(0,7);
-
-            res.status(200).json(theReturnedArticles);
-         });
-
+         return {
+            from: makeFriendlyDate(fromDate),
+            to: makeFriendlyDate(toDate)
+         };
       };
 
-      getRealNews(theKeywords);
+      // for now, get news based on first two keywords, this algorithm will be tweaked
+      let query = theKeywords.slice(0, 3).join(" ");
+      let dates = getFromAndToDateStrings(31);
+      console.log("the dates are:\n", dates, " and the query is:\n", query);
+ 
+      newsapi.v2.everything({
+         q: query,
+         from: dates.from,
+         to: dates.to,
+         language: 'en',
+         sortBy: 'relevancy',
+         page: 1
+      }).then(response => {
+         let theReturnedArticles = response.articles.slice(0, articleLimit);
+         res.status(200).json(theReturnedArticles);
+      });
+
+
+
+
 
    }
-
 
 };
