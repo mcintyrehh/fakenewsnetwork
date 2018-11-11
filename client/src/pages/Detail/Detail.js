a35ea1125ffbb248d3f7cab7f16615aed1a69c8b
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-11-10 19:11:45 -0500

    Added Tabs to detail page, now need to add content to 'blue pill tab'

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Tabs } from 'antd';
 import Wrapper from '../../components/Wrapper';
 import { Row, Col } from 'antd';
 import DetailCard from '../../components/DetailCard';
@@ -10,7 +10,7 @@ import './Detail.css';
 import API from '../../utils/API'
 import axios from 'axios';
 const { Content } = Layout;
-
+const TabPane = Tabs.TabPane;
 class Detail extends Component {
     
     emitEmpty = () => {
@@ -21,6 +21,7 @@ class Detail extends Component {
         super(props);
         this.state = {
             redirectTo: null,
+            tabColor: 'red',
             outerColWidth: 6,
             innerColWidth: 10,
             fakeNews:{},
@@ -76,7 +77,12 @@ class Detail extends Component {
           .catch(function (error) {
             console.log(error);
           })
-	}
+    }
+
+    changeColor = () => {
+        (this.state.tabColor === 'red') ? this.setState({tabColor: 'cyan'}) : this.setState({tabColor: 'red'});
+    }
+    
     render() {
         console.log("Article Object", this.state.fakeNews);
         return (
@@ -91,11 +97,28 @@ class Detail extends Component {
                             <Col span={this.state.innerColWidth}>
                                 <DetailCard user={this.props.user} title={this.state.fakeNews.title} id={this.state.fakeNews._id}articleType={this.state.fakeNews.articleType} img={this.state.fakeNews.src} url={this.state.fakeNews.url} summary={this.state.fakeNews.summary}/> 
                             </Col>
-                            <Col span={this.state.innerColWidth}> 
+                        </Row>
+                        <Row style={{ textAlign: 'center', color: 'white' }}>
+                            {/* <Col span={this.state.innerColWidth}>  */}
+                            <div>
+                                <Tabs
+                                defaultActiveKey="1"
+                                tabPosition="right"
+                                style={{ height: 220, marginTop: "5vh", color: 'white' }}
+                                tabBarStyle={{color: this.state.tabColor}}
+                                onChange={this.changeColor}
+                                >
+                                    <TabPane tab="Blue Pill" key="1">Content of tab 1</TabPane>
+
+                                    {/* Real Article Cards Go Here */}
+                                    <TabPane tab="Red Pill" key="2">Content of tab 2</TabPane>
+                                
+                                </Tabs>
+                            </div>
                             
                             {/*{this.state.realNews && this.state.realNews.map(real=> <RealCard real={real} key={real.id}></RealCard>)} */}
-                            </Col>
-                            <Col span={this.state.outerColWidth}></Col>
+                            {/* </Col> */}
+                            {/* <Col span={this.state.outerColWidth}></Col> */}
                         </Row>
                     </Content>
                 </Layout>
