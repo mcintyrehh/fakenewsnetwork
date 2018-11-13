f46107cefe5dd861f8ef813360ffc598148b79d2
Onray <mcintyrehh@gmail.com>
2018-11-13 12:22:01 -0500

    updating production environment route to serve index.html from / to /*

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(passport.session()); // will call the deserializeUser
 if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV');
 	app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
-	app.get('/', (req, res) => {
+	app.get('/*', (req, res) => {
 		res.sendFile(path.join(__dirname, '../client/build/'));
 	});
 }
