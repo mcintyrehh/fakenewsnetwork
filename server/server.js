8533137340c5a6d7317af44a9e77c7fd070efd4f
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-11-13 19:34:00 -0500

    changed production logic in server.js, heroku ts

--- server/server.js
+++ server/server.js
@@ -37,9 +37,9 @@ app.use(passport.session()); // will call the deserializeUser
 // If its production environment!
 if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV');
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../', 'client/build')));
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, './client/build/'));
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
 
